9c2fccc541bdf6c72f205dee92f21d50d8461b85
mguvensncktr <mguvensancaktar@gmail.com>
2022-02-21 18:33:55 +0300

    coin info

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,16 +1,27 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-import { Ionicons, EvilIcons } from '@expo/vector-icons';
-
+import { Ionicons, EvilIcons, AntDesign } from '@expo/vector-icons';
 import coin from '../assets/data/crypto.json';
 
 const DetailScreen = () => {
+
     const { image: { small },
-        name,
         symbol,
-        market_data: { market_cap_rank },
+        name,
+        market_data: {
+            market_cap_rank,
+            current_price,
+            price_change_percentage_24h
+        },
     }
         = coin;
+
+    const formatPrice = (price) => {
+        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    const priceColor = price_change_percentage_24h > 0 ? '#16c784' : '#ea3943'
+    const priceImage = price_change_percentage_24h > 0 ? 'caretup' : 'caretdown'
 
     function renderHeader() {
         return (
@@ -49,9 +60,42 @@ const DetailScreen = () => {
         )
     }
 
+    function renderCoinInfo() {
+        return (
+            <View
+                style={{
+                    margin: 15,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}
+            >
+                <View>
+                    <Text style={{ fontSize: 22, color: 'white' }}>{name}</Text>
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 24, letterSpacing: 1 }}>{formatPrice(current_price.usd)} US$</Text>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: priceColor,
+                        padding: 5,
+                        borderRadius: 10,
+                    }}
+                >
+                    <AntDesign name={priceImage} size={12} color="#fff" />
+                    <Text style={{ color: 'white', fontSize: 16, marginLeft: 5, fontWeight: 'bold' }}>{price_change_percentage_24h.toFixed(2)} %</Text>
+                </View>
+            </View>
+        )
+    }
+
+
     return (
         <View style={{ flex: 1 }}>
             {renderHeader()}
+            {renderCoinInfo()}
         </View>
     )
 }
